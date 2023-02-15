04dd560c3e13bf6448db7a429727f0f9e48e1823
yusuke1997 <63439062+yusuke1997@users.noreply.github.com>
2023-02-16 04:57:51 +0900

    add rus dict[

--- translit_tt/transfer.py
+++ translit_tt/transfer.py
@@ -13,22 +13,22 @@ class trans:
         yml_files=glob.glob(f'{path}/alphabet/*.yml')
         assert len(yml_files)!=0,'Any yaml file is not exist'
 
-        self.alphabets_list=dict()
-        for file in yml_files:
-            assert os.path.exists(file),'{} is not exist'.format(file)
-            try:
-                with open(file, 'r') as f:
-                    yml = yaml.safe_load(f)
-                    self.alphabets_list[file.rsplit('/',1)[-1].rsplit('.',1)[0]] = yml
-                    
-            except Exception as e:
-                print('Exception occurred while loading YAML...',file=sys.stderr)
-                print(e, file=sys.stderr)
-                sys.exit(1)
+        self.alphabets_list= dict()
+        self.alphabets_list['rus'] = {'Ё': 'Yo', 'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G','Д': 'D,'
+                                      'Е': 'E', 'Ж': 'J', 'З': 'Z', 'И': 'İ', 'Й': 'Y', 'К': 'K',
+                                      'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R',
+                                      'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'X', 'Ц': 'Ts',
+                                      'Ч': 'Ç', 'Ш': 'Ş', 'Щ': 'Şç', 'Ъ': '', 'Ь': '', 'Ы': 'I',
+                                      'Э': 'E', 'Ю': 'Yu','Я': 'Ya','а': 'a', 'б': 'b', 'в': 'v'.
+                                      'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'j', 'з': 'z', 'и': 'i',
+                                      'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+                                      'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f',
+                                      'х': 'x', 'ц': 'ts', 'ч': 'ç', 'ш': 'ş', 'щ': 'şç', 'ъ': '',
+                                      'ы': 'ı', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya', 'ё': 'yo',
+                                      'ия': 'iyä'}
 
-        self.alphabets_list['tat'] = 'this is unknown'
+        self.alphabets_list['tat'] = None'this is unknown'
         self.tatar = tatar_trans()
-
 
     def tokenize(self,text,lang):
         new_text = list(text)
